beb0d6fef99f3f1e416283c591cdc96b3cedff74
WilderSig <gonzalez5664@gmail.com>
2017-03-15 23:25:04 -0600

    HOJA DE TRABAJO 2 ARREGLADA

--- src/org/compi2/codigo3d/parser/ControlC3D.java
+++ src/org/compi2/codigo3d/parser/ControlC3D.java
@@ -8,6 +8,9 @@ public class ControlC3D {
     
     private static int temp = 0;
     private static String c3d = "";
+    private static String verdaderas = "";
+    private static String falsas = "";
+    private static int tempeti = 0;
     
     /**
      * Reinicia las variables estáticas relacionadas con la generación del
@@ -15,7 +18,10 @@ public class ControlC3D {
      */
     public static void reiniciar(){
         temp = 0;
+        tempeti = 0;
         c3d = "";
+        verdaderas = "";
+        falsas = "";
     }
     
     /**
@@ -23,9 +29,12 @@ public class ControlC3D {
      * @return El siguiente temporal (t$#)
      */
     public static String generaTemp(){
-        return "t$"+temp++;
+        return "t"+temp++;
     }
     
+    public static String generaEtiqueta(){
+        return "L" + tempeti++;
+    }
     /**
      * Agrega la sentencia que recibe como parámetro a la cadena de código
      * de tres direcciones que se va generando hasta ese momento.
@@ -35,12 +44,33 @@ public class ControlC3D {
         c3d += sentencia;
     }
     
+    public static void agregarEtiquetas(String Verdadera, String Falsa){
+        if(!"".equals(verdaderas)){
+            verdaderas += "," + Verdadera;    
+        }else{
+            verdaderas = Verdadera;
+        }
+        
+        if(!"".equals(falsas)){
+           falsas += "," + Falsa; 
+        }else{
+           falsas = Falsa;
+        }
+    }
     /**
      * Devuelve el código 3D generao hasta el momento de su invocación.
      * @return Código 3D generado
      */
     public static String getC3D(){
-        return c3d;
+        if(falsas.equals("") || verdaderas.equals("")){
+            c3d += "//ultimo valor: t"+(temp-1);
+            return c3d;
+        }else{
+            c3d += "//Etiquetas de Verdadero: "+ verdaderas + "\n";
+            c3d += "//Etiquetas de Falso: "+ falsas + "\n";
+            return c3d;
+        }
+        
     }
     
 }
--- src/org/compi2/codigo3d/parser/NodoC3D.java
+++ src/org/compi2/codigo3d/parser/NodoC3D.java
@@ -8,13 +8,45 @@ package org.compi2.codigo3d.parser;
  */
 public class NodoC3D {
     private String cad;
+    private String etV;
+    private String etF;
 
     public NodoC3D(String cad) {
         this.cad = cad;
+        this.etF = "";
+        this.etV = "";
     }
     
     public String getCad(){
         return cad;
     }
+
+    public NodoC3D(String cad, String etV, String etF) {
+        this.cad = cad;
+        this.etV = etV;
+        this.etF = etF;
+    }
+
+    public String getEtV() {
+        return etV;
+    }
+
+    public String getEtF() {
+        return etF;
+    }
+
+    public void setCad(String cad) {
+        this.cad = cad;
+    }
+
+    public void setEtV(String etV) {
+        this.etV = etV;
+    }
+
+    public void setEtF(String etF) {
+        this.etF = etF;
+    }
+    
+    
         
 }
